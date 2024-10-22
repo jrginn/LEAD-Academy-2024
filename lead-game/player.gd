945726f921a406525da058686406aeb01b1bc755
Gavin Hewitt <ghewitt@email.sc.edu>
2024-10-21 22:54:39 -0400

    collision detection

--- lead-game/player.gd
+++ lead-game/player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -20,18 +20,21 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-
+	
+	$AnimatedSprite2D.play()
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
 		
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	#position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0:
+	if velocity.x != 0 or velocity.y != 0: 
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	if velocity.length() == 0:
+		$AnimatedSprite2D.animation = "idle"
+		
