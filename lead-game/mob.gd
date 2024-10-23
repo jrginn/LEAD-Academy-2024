39b1afea0e806591716ed3d0e902c75d3d2cb4be
Gavin Hewitt <ghewitt@email.sc.edu>
2024-10-23 00:05:05 -0400

    enemy player detection

--- lead-game/mob.gd
+++ lead-game/mob.gd
@@ -7,13 +7,17 @@ var y_velocity_sign = 1
 
 enum States {IDLE, RUN}
 
+var playerBody: CharacterBody2D
+var playerDetected: bool
+
 func _ready():
 	$MovementTimer.start()
 
-func _physics_process(delta):
-	velocity.x = x_velocity_sign * speed
-	velocity.y = y_velocity_sign * speed
-	move_and_slide()
+func _physics_process(_delta):
+	if playerDetected:
+		velocity.x = x_velocity_sign * speed
+		velocity.y = y_velocity_sign * speed
+		move_and_slide()
 
 func _on_movement_timer_timeout() -> void:
 	if randi() % 2:
@@ -25,3 +29,18 @@ func _on_movement_timer_timeout() -> void:
 		y_velocity_sign = 1
 	else:
 		y_velocity_sign = -1
+
+
+func _on_detection_zone_body_entered(body: Node2D):
+	if "Wizard" in body.name or "Archer" in body.name:
+		playerBody = body
+		playerDetected = true
+		$AnimatedSprite2D.animation = "run"
+		$AnimatedSprite2D.play()
+
+
+func _on_detection_zone_body_exited(body: Node2D) -> void:
+	if "Wizard" in body.name or "Archer" in body.name:
+		playerBody = null
+		playerDetected = false
+		$AnimatedSprite2D.animation = "idle"
