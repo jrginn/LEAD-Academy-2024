fe594ccb37740980c3dbf003d62798b5a4879476
jrginn <jrginn47@gmail.com>
2024-10-23 22:13:02 -0400

    timed fireball to animation

--- lead-game/player.gd
+++ lead-game/player.gd
@@ -32,14 +32,6 @@ func _physics_process(delta):
 	
 	if is_attacking: return # do not change animations before attack has completed
 	
-	#if is_facing_right && rotation == PI:
-		#rotation += PI
-		#scale.y = 1
-	#elif not is_facing_right && rotation == 0:
-		#rotation += PI
-		#print(rotation)
-		#scale.y = -1
-	
 	if Input.is_action_pressed("left_click"):
 		state = States.ATTACK
 		shoot()
@@ -47,15 +39,6 @@ func _physics_process(delta):
 		state = States.WALK
 	else:
 		state = States.IDLE
-	#look_at(get_global_mouse_position())
-	#if get_global_mouse_position().x > position.x:
-		#flip_h = false
-	#elif get_global_mouse_position().x < position.x:
-		#flip_h = true
-	#if velocity.x < 0:
-		#scale = Vector2(-1,1)
-	#elif velocity.x > 0:
-		#scale = Vector2(1,1)
 
 func set_state(new_state: int):
 	state = new_state
@@ -64,11 +47,6 @@ func set_state(new_state: int):
 		$AnimatedSprite2D.animation = "idle"
 	elif state == States.WALK:
 		$AnimatedSprite2D.animation = "walk"
-		#$AnimatedSprite2D.flip_h = not is_facing_right
-		#if is_facing_right:
-			#$ShootPoint.position.x = abs($ShootPoint.position.x)  # Ensure it's on the right
-		#else:
-			#$ShootPoint.position.x = -abs($ShootPoint.position.x)  # Flip to the left
 
 	elif state == States.ATTACK:
 		is_attacking = true
@@ -80,8 +58,9 @@ func _on_attack_finished():
 	state = States.IDLE
 	
 func shoot():
+	await get_tree().create_timer(.2).timeout
 	var p = Projectile.instantiate()
 	get_tree().current_scene.add_child(p)
-	#p.look_at(get_global_mouse_position())
 	p.transform = $ShootPoint.global_transform
+	p.scale = Vector2(4,4)
 	
--- lead-game/projectile.gd
+++ lead-game/projectile.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var speed = 75
+var speed = 100
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
